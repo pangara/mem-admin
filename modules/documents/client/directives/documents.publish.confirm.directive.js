f98f79c06f7fd6edddfe4a670f89787d7c063775
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-07-10 16:19:02 -0700

    MEM-412. Do not allow publish document if any parent folder is not published

--- modules/documents/client/directives/documents.publish.confirm.directive.js
+++ modules/documents/client/directives/documents.publish.confirm.directive.js
@@ -6,6 +6,7 @@ angular.module('documents')
 		restrict: 'A',
 		scope: {
 			docs: '=',
+			currentNode: "=", // current parent folder
 			publisher: '='
 		},
 		link: function (scope, element, attrs) {
@@ -29,27 +30,48 @@ angular.module('documents')
 					// caller may have batch or single file
 					self.docs = Array.isArray(scope.docs) ? scope.docs : [ scope.docs ];
 					self.publisher = scope.publisher;
+					self.currentPath = scope.currentNode.getPath();
 					self.submit = submit;
 					self.cancel = cancel;
 					// collect files that can and can't be published
 					self.publishableFiles = [];
 					self.missingCategory = [];
-					_.each(self.docs, function(o) {
-						if (o.userCan.publish) {
-							if (o.documentCategories && o.documentCategories.length > 0) {
-								self.publishableFiles.push(o);
-							} else {
-								self.missingCategory.push(o);
-							}
+					self.showSubmit = true;
+					self.cancelText = 'No';
+					// collect unpublished folders on path.
+					var unpublishedFolders = _.filter(self.currentPath, function(p) { return ! p.model.published; });
+					if (unpublishedFolders.length > 0) {
+						self.showSubmit = false;
+						self.cancelText = 'OK';
+						var fldNames = _.map(unpublishedFolders, function (fld) { return fld.model.name; });
+						var last = fldNames.pop();
+						var prefix = fldNames.length > 0 ? fldNames.join(', ') + " and " : '';
+						var msg = prefix + last;
+						var suffix = self.docs.length > 1 ? ' to publish these documents.' : ' to publish this document';
+						if (fldNames.length > 0) {
+							self.errMsg = "You need to publish parent folders " + msg + suffix;
+						} else {
+							self.errMsg = "You need to publish parent folder " + msg + suffix;
 						}
-					});
-					if ( self.publishableFiles.length === 0) {
-						self.errMsg = 'No files can be published';
+					} else {
+						_.each(self.docs, function (o) {
+							if (o.userCan.publish) {
+								if (o.documentCategories && o.documentCategories.length > 0) {
+									self.publishableFiles.push(o);
+								} else {
+									self.missingCategory.push(o);
+								}
+							}
+						});
+
+						if (self.publishableFiles.length === 0) {
+							self.errMsg = 'No files can be published';
+						}
+						self.confirmText =
+							self.publishableFiles.length > 1 ?
+								'Are you sure you want to publish the selected files?' :
+								'Are you sure you want to publish the selected file?';
 					}
-					self.confirmText =
-						self.publishableFiles.length > 1 ?
-							'Are you sure you want to publish the selected files?' :
-							'Are you sure you want to publish the selected file?';
 
 					function cancel() {
 							$modalInstance.dismiss('cancel');
