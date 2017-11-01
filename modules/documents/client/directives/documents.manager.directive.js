6e8ae45456dca084db39a090de806c10346c1cfd
pangara <priya.angara@gov.bc.ca>
2017-11-01 15:43:26 -0700

    Deleting a document does not update its associated collections

--- modules/documents/client/directives/documents.manager.directive.js
+++ modules/documents/client/directives/documents.manager.directive.js
@@ -433,11 +433,13 @@ angular.module('documents')
 				};
 
 				self.deleteDocument = function(documentID) {
+					var collections = null;
 					return Document.lookup(documentID)
-						.then( function (doc) {
+						.then(function(doc) {
+							collections = doc.collections;
 							return Document.getProjectDocumentVersions(doc._id);
 						})
-						.then( function (docs) {
+						.then(function(docs) {
 							// Are there any prior versions?  If so, make them the latest and then delete
 							// otherwise delete
 							if (docs.length > 0) {
@@ -446,30 +448,23 @@ angular.module('documents')
 								return null;
 							}
 						})
-						.then( function () {
-							// Delete it from the system.
+						.then(function() {
+							// MEM-597: If there are there any collections associated with this document,
+							// then delete the collection document reference in the collections as well.
+							var promises = _.union(_.map(collections, function(c) {
+								return CollectionModel.removeOtherDocument(c._id, documentID);
+							}), _.map(collections, function(c) {
+								return CollectionModel.removeMainDocument(c._id, documentID);
+							}));
+							return Promise.all(promises);
+						})
+						.then(function () {
+							// Delete the document from the system.
 							return Document.deleteDocument(documentID);
+						})
+						.catch (function(err) {
+							AlertService.error('The document could not be deleted.');
 						});
-				};
-
-				self.deleteDocument = function(documentID) {
-					return Document.lookup(documentID)
-					.then( function (doc) {
-						return Document.getProjectDocumentVersions(doc._id);
-					})
-					.then( function (docs) {
-						// Are there any prior versions?  If so, make them the latest and then delete
-						// otherwise delete
-						if (docs.length > 0) {
-							return Document.makeLatestVersion(docs[docs.length-1]._id);
-						} else {
-							return null;
-						}
-					})
-					.then( function () {
-						// Delete it from the system.
-						return Document.deleteDocument(documentID);
-					});
 				};
 
 				self.deleteSelected = {
