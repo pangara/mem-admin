107dd1d0c87bd2e9905ee0e834f25622b6c73a97
Angara <PANGARA@BCGOV>
2017-12-05 11:12:05 -0800

    Add pinned versions of packages

--- config/lib/express.js
+++ config/lib/express.js
@@ -167,9 +167,9 @@ module.exports.initModulesConfiguration = function (app, db) {
 module.exports.initHelmetHeaders = function (app) {
 	// Use helmet to secure Express headers
 	var SIX_MONTHS = 15778476000;
-	app.use(helmet.xframe());
+	app.use(helmet.frameguard());
 	app.use(helmet.xssFilter());
-	app.use(helmet.nosniff());
+	app.use(helmet.noSniff());
 	app.use(helmet.hsts({
 		maxAge: SIX_MONTHS,
 		includeSubdomains: true,
